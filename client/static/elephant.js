2b22a8a936b86dfdc79827b51f525e254ca3b11c
Jeffrey Wang <j@mywikis.com>
2016-08-06 17:54:26 -0500

    Adding zip code and cookies support

--- client/static/elephant.js
+++ client/static/elephant.js
@@ -1,16 +1,27 @@
 // Setup script on document load
 $(function() {
   console.log("elephant client v0.0.1");
+  /*
   // get geodata
   if ("geolocation" in navigator) {
-    /* geolocation is available */
+    // geolocation is available
     console.log("Geolocation available.");
     navigator.geolocation.getCurrentPosition(function(position) {
       alert(position.coords.latitude + " " + position.coords.longitude);
     });
   } else {
-    /* geolocation IS NOT available */
+    // geolocation IS NOT available
     console.log("Geolocation not available.");
+  }
+  */
+  // store/check cookie in zip code
+  var zip = document.cookie.replace(/(?:(?:^|.*;\s*)elephant_zipcode\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  if ( zip == undefined || zip == null || zip == "" ) {
+    givenzip = prompt("What is your zip code?");
+    document.cookie = "elephant_zipcode=" + givenzip;
+    console.log("Zip code stored as " + givenzip);
+  } else {
+    alert(zip);
   }
   // send geodata, pull stuff from server
 
