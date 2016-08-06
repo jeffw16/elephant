3ee298bb55e4637a2bbde04f48354d1625530c47
Trevor Thai Kim Nguyen <trevortknguyen@gmail.com>
2016-08-06 17:30:55 -0500

    NLTK testing code and gitignore for virtualenvs

--- nlp/nlpserver.py
+++ nlp/nlpserver.py
@@ -1,2 +1,11 @@
 # This is the Python server that will process all of the questions
 # It is supposed to link duplicate questions together
+
+import nltk
+
+sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
+tokens = nltk.word_tokenize(sentence)
+print tokens
+
+tagged = nltk.pos_tag(tokens)
+print tagged[:6]
